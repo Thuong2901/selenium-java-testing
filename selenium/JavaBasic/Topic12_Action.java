919952e333b808d596ad3cc2b0c7c8231ef86fa2
thuong2901 <thuong29011997@gmail.com>
2025-08-27 18:08:46 +0700

    Action III

---
+++ selenium/JavaBasic/Topic12_Action.java
@@ -0,0 +1,13 @@
+package JavaBasic;
+
+import java.sql.SQLOutput;
+
+public class Topic12_Action {
+    public static void main(String args){
+        String osName = System.getProperty("os.name");
+        String projectPath = System.getProperty("user.dir");
+        System.out.println(osName);
+        System.out.println(projectPath);
+
+    }
+}
